f0f70595e4bd74c12865f13b93ad2ac0142d7c99
BeC9_gewei <grey@pku.edu.cn>
2024-11-03 00:47:02 +0800

    add sign function

--- src/utils.rs
+++ src/utils.rs
@@ -2,3 +2,13 @@ pub fn is_nan_or_inf(x: f64) -> bool {
     // x - x != 0.0
     x.is_nan() || x.is_infinite()
 }
+
+pub fn sign<T: PartialOrd + From<i8>>(x: T) -> i8 {
+    if x > T::from(0) {
+        1
+    } else if x < T::from(0) {
+        -1
+    } else {
+        0
+    }
+}
