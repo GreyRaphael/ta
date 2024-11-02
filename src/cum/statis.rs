6a6efd14bdc2993f78720da417401d0eba1a33c4
BeC9_gewei <grey@pku.edu.cn>
2024-11-03 01:52:21 +0800

    finish Kurter of cum

--- src/cum/statis.rs
+++ src/cum/statis.rs
@@ -140,3 +140,52 @@ impl Skewer {
         }
     }
 }
+
+#[pyclass]
+pub struct Kurter {
+    count: f64,
+    sumer: Sumer,
+    sq_sumer: Sumer,
+    cub_sumer: Sumer,
+    quad_sumer: Sumer,
+}
+
+#[pymethods]
+impl Kurter {
+    #[new]
+    pub fn new() -> Self {
+        Self {
+            count: 0.0,
+            sumer: Sumer::new(),
+            sq_sumer: Sumer::new(),
+            cub_sumer: Sumer::new(),
+            quad_sumer: Sumer::new(),
+        }
+    }
+
+    pub fn update(&mut self, new_val: f64) -> f64 {
+        if is_nan_or_inf(new_val) {
+            NAN
+        } else {
+            self.count += 1.0;
+
+            let quad_sum = self.quad_sumer.update(new_val.powi(4));
+            let cub_sum = self.cub_sumer.update(new_val.powi(3));
+            let sq_sum = self.sq_sumer.update(new_val.powi(2));
+            let sum = self.sumer.update(new_val);
+            let mean = sum / self.count as f64;
+            let variance = (sq_sum - 2.0 * sum * mean + self.count as f64 * mean.powi(2))
+                / (self.count as f64 - 1.0);
+
+            let value = quad_sum - 4.0 * cub_sum * mean + 6.0 * sq_sum * mean.powi(2)
+                - 4.0 * sum * mean.powi(3)
+                + self.count as f64 * mean.powi(4);
+
+            let n = self.count;
+            let factor01 = n * (n + 1.0) / ((n - 1.0) * (n - 2.0) * (n - 3.0));
+            let factor02 = (n - 1.0) * (n - 1.0) / ((n - 2.0) * (n - 3.0));
+
+            factor01 * value / variance.powi(2) - factor02
+        }
+    }
+}
