d3c29e22f1d91660bda0521f1c4292f7717b22b7
BeC9_gewei <grey@pku.edu.cn>
2024-11-02 22:45:55 +0800

    fix deltaer

--- src/rolling/delta.rs
+++ src/rolling/delta.rs
@@ -4,9 +4,8 @@ use std::f64::NAN;
 #[pyclass]
 pub struct Deltaer {
     buf: Vec<f64>,
-    pub n: usize,
-    pub cur_idx: usize,
-    pub head_idx: usize,
+    cur_idx: usize,
+    head_idx: usize,
 }
 
 #[pymethods]
@@ -15,7 +14,6 @@ impl Deltaer {
     pub fn new(n: usize) -> Self {
         Self {
             buf: vec![NAN; n],
-            n,
             cur_idx: 0,
             head_idx: 0,
         }
@@ -24,15 +22,19 @@ impl Deltaer {
     pub fn update(&mut self, new_val: f64) -> f64 {
         self.head_idx = self.cur_idx;
         self.buf[self.head_idx] = new_val;
-        self.cur_idx = (self.cur_idx + 1) % self.n;
+        self.cur_idx = (self.cur_idx + 1) % self.buf.len();
 
         new_val - self.buf[self.cur_idx]
+    }
+
+    pub fn get(&self, idx: usize) -> f64 {
+        self.buf[(self.head_idx + idx) % self.buf.len()]
     }
 }
 
 #[pyclass]
 pub struct Pctchanger {
-    pub deltaer: Deltaer,
+    deltaer: Deltaer,
 }
 
 #[pymethods]
@@ -47,5 +49,9 @@ impl Pctchanger {
     pub fn update(&mut self, new_val: f64) -> f64 {
         let diff = self.deltaer.update(new_val);
         diff / (new_val - diff)
+    }
+
+    pub fn get(&self, idx: usize) -> f64 {
+        self.deltaer.get(idx)
     }
 }
