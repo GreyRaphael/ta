8fd6478a4376d1803a5b73a25ca126b4e4900fdc
BeC9_gewei <grey@pku.edu.cn>
2024-11-03 00:53:07 +0800

    add variadic max min sum multiple

--- src/utils.rs
+++ src/utils.rs
@@ -12,3 +12,59 @@ pub fn sign<T: PartialOrd + From<i8>>(x: T) -> i8 {
         0
     }
 }
+
+#[macro_export]
+macro_rules! sum {
+    ($($x:expr),*) => {
+        {
+            let mut temp_sum = 0.0;
+            $(
+                temp_sum += $x as f64;
+            )*
+            temp_sum
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! multiply {
+    ($($x:expr),*) => {
+        {
+            let mut temp_multiply = 0.0;
+            $(
+                temp_multiply *= $x as f64;
+            )*
+            temp_multiply
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! max {
+    ($($x:expr),*) => {
+        {
+            let mut max_val = std::f64::NEG_INFINITY;
+            $(
+                if $x as f64 > max_val {
+                    max_val = $x as f64;
+                }
+            )*
+            max_val
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! min {
+    ($($x:expr),*) => {
+        {
+            let mut min_val = std::f64::INFINITY;
+            $(
+                if $x as f64 < min_val {
+                    min_val = $x as f64;
+                }
+            )*
+            min_val
+        }
+    };
+}
