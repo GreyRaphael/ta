52736f656ab53baea679d83c61f075172d541149
BeC9_gewei <grey@pku.edu.cn>
2024-11-02 22:21:42 +0800

    fix sum.rs

--- src/rolling/sum.rs
+++ src/rolling/sum.rs
@@ -5,7 +5,6 @@ use std::f64::NAN;
 #[pyclass]
 pub struct Sumer {
     buf: Vec<f64>,
-    pub n: usize,
     cur_idx: usize,
     nan_count: usize,
     sum: f64,
@@ -17,7 +16,6 @@ impl Sumer {
     pub fn new(n: usize) -> Self {
         Self {
             buf: vec![NAN; n],
-            n,
             cur_idx: 0,
             nan_count: n,
             sum: 0.0,
@@ -27,7 +25,7 @@ impl Sumer {
     pub fn update(&mut self, new_val: f64) -> f64 {
         let old_val = self.buf[self.cur_idx];
         self.buf[self.cur_idx] = new_val;
-        self.cur_idx = (self.cur_idx + 1) % self.n;
+        self.cur_idx = (self.cur_idx + 1) % self.buf.len();
 
         if is_nan_or_inf(new_val) {
             self.nan_count += 1;
@@ -51,7 +49,7 @@ impl Sumer {
 
 #[pyclass]
 pub struct Meaner {
-    pub sumer: Sumer,
+    sumer: Sumer,
 }
 
 #[pymethods]
@@ -64,7 +62,6 @@ impl Meaner {
     }
 
     pub fn update(&mut self, new_val: f64) -> f64 {
-        let sum = self.sumer.update(new_val);
-        sum / self.sumer.n as f64
+        self.sumer.update(new_val) / self.sumer.buf.len() as f64
     }
 }
