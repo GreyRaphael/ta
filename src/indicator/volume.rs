be1d747ac126bfe557a2e6f90d59e1aa3e563bb7
KTC9_gewei <grey@pku.edu.cn>
2024-11-05 09:14:37 +0800

    finish Chaikin A/D Oscillator

--- src/indicator/volume.rs
+++ src/indicator/volume.rs
@@ -1,3 +1,4 @@
+use super::overlap::EMA;
 use crate::rolling;
 use pyo3::prelude::*;
 
@@ -21,5 +22,31 @@ impl AD {
         let mfv = mfm * volume as f64;
 
         self.sumer.update(mfv)
+    }
+}
+
+// Chaikin A/D Oscillator
+#[pyclass]
+pub struct ADOSC {
+    ema_fast: EMA,
+    ema_slow: EMA,
+    ader: AD,
+}
+
+#[pymethods]
+impl ADOSC {
+    #[new]
+    pub fn new(timeperiod: usize, fastperiod: usize, slowperiod: usize) -> Self {
+        Self {
+            ema_fast: EMA::new(fastperiod),
+            ema_slow: EMA::new(slowperiod),
+            ader: AD::new(timeperiod),
+        }
+    }
+
+    pub fn update(&mut self, high: f64, low: f64, close: f64, volume: usize) -> f64 {
+        let ad_value = self.ader.update(high, low, close, volume);
+
+        self.ema_fast.update(ad_value) - self.ema_slow.update(ad_value)
     }
 }
